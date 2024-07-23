0c108969e7367f4d84ce6455e9668834f5613e13
Carlos <carlosestupina@gmail.com>
2024-07-23 15:50:01 +0200

    ejercicios 5 y 6

--- ejercicio5.sql
+++
@@ -1,1 +0,0 @@
-GRANT SELECT, UPDATE on Books TO  'martin@localhost'; 
---
+++ ejercicios5y6.sql
@@ -0,0 +1,8 @@
+--ejercicio 5
+GRANT SELECT, UPDATE on Books TO  'martin'@'localhost'; 
+
+
+
+--ejercicio 6
+
+REVOKE UPDATE on Books FROM  'martin'@'localhost'; 
